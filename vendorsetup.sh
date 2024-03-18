db3ade5302d813ebf7458dfaff6c3a4566d011a4
nekoprjkt <maseadek1986@gmail.com>
2024-03-18 10:38:06 +0000

    ysl: add vendorsetup
    Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone --depth=1 https://github.com/nekoprjkt/vendor-xiaomi-ysl -b 13 vendor/xiaomi/ysl
+git clone --depth=1 https://github.com/nekoprjkt/device-xiaomi-msm8953-common -b 13 device/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/nekoprjkt/vendor-xiaomi-msm8953-common -b 13 vendor/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/nekoprjkt/kernel-xiaomi-msm8953 -b 13 kernel/xiaomi/msm8953
